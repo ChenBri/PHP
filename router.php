a2c7473224a623f316dcd983e12b7e1633ab4dcb
Chen <chenbrilling@gmail.com>
2024-09-06 16:30:35 +0300

    Added styling

--- router.php
+++ router.php
@@ -1,22 +1,28 @@
-<?php
-$uri = substr($_SERVER["REQUEST_URI"], 12);
-$uri = parse_url($uri)["path"];
+<div class="p-12">
 
-switch ($uri) {
-    case '/home':
-        require 'controllers/home-controller.php';
-        break;
-    case '/about':
-        require 'controllers/about-controller.php';
-        break;
+    <?php
+    $uri = substr($_SERVER["REQUEST_URI"], 12);
+    $uri = parse_url($uri)["path"];
 
-    case '/contact':
-        require 'controllers/contact-controller.php';
-        break;
-    case '/notes':
-        require 'controllers/notes-controller.php';
-        break;
-    default:
-        require 'controllers/home-controller.php';
-        break;
-}
+    switch ($uri) {
+        case '/home':
+            require 'controllers/home-controller.php';
+            break;
+        case '/about':
+            require 'controllers/about-controller.php';
+            break;
+
+        case '/contact':
+            require 'controllers/contact-controller.php';
+            break;
+        case '/notes':
+            require 'controllers/notes-controller.php';
+            break;
+        default:
+            require 'views/error-view.php';
+            break;
+    }
+
+    ?>
+
+</div>
---
+++ views/error-view.php
@@ -0,0 +1,6 @@
+<div>
+    <h1 class="text-2xl  font-bold">Page not found</h1>
+    <p>
+        <a href="/home" class="text-lg text-blue-500 underline"> Click here to return home.</a>
+    </p>
+</div>
