e67acd787a1b9f04e5f9fc7615dee4f7f6093b7d
Chen <chenbrilling@gmail.com>
2024-09-08 13:29:21 +0300

    Added note creation

--- controllers/notes-controller.php
+++ controllers/notes-controller.php
@@ -5,15 +5,31 @@ $content = "Welcome to the notes view";
 
 $db = new Database($db_config['host'], $db_config['db'], $db_config['user'], $db_config['pass'], $db_config['port']);
 
-
-$notes = $db->query("SELECT * FROM notes WHERE user_id = 1");
 // If user is not logged in || login
-
+/* if(true){
+    require "views/login-view.php";
+} */
 // If user is not logged in || register
 
 // If user is logged in
 // Fetch all notes of the users
 
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    
+
+    $db->query(
+        'INSERT INTO notes(header,body,user_id) VALUES(:body, :header, :user_id)',
+        [
+            'header' => $_POST['header'],
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]
+    );
+}
+
+
+$notes = $db->query("SELECT * FROM notes WHERE user_id = 1");
+
 require "views/notes-view.php";
-
-
--- views/notes-view.php
+++ views/notes-view.php
@@ -5,7 +5,18 @@ require('components/content.php');
 ?>
 
 <div>
-    <h1 class="text-2xl text-bold">Your Notes:</h1>
+    <h2>Add a new note:</h2>
+    <form method="POST">
+        <label for="header">Header</label>
+        <input type="text" name="header" placeholder="Note's Header" />
+        <label for="Body">Body</label>
+        <input type="text" name="body" placeholder="Body's Header" />
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <br />
+    <h2 class="text-2xl text-bold">Your Notes:</h2>
     <ul>
         <?php foreach ($notes as $note): ?>
             <li><?= $note['header'] ?> - <?= $note['body'] ?></li>
