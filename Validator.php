17d2c356ec68b60275c1a809ebc6855091e03774
Chen <chenbrilling@gmail.com>
2024-09-17 00:51:02 +0300

    Added validations

--- Validator.php
+++ Validator.php
@@ -1,12 +1,38 @@
-<?php 
-class Validator 
+<?php
+class Validator
 {
-    public static function string($str){
+    public static function string($str)
+    {
         $trimmed_str = trim($str);
         return strlen($trimmed_str) === 0 || strlen($trimmed_str) > 1000;
     }
 
-    public static function email($email){
+    public static function email($email)
+    {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function password($password)
+    {
+        return strlen($password) >= 8 && preg_match('/[A-Za-z]/', $password) && preg_match('/[0-9]/', $password);
+    }
+
+    public static function integer($number, $min = null, $max = null)
+    {
+        if (filter_var($number, FILTER_VALIDATE_INT) === false) {
+            return false;
+        }
+        if ($min !== null && $number < $min) {
+            return false;
+        }
+        if ($max !== null && $number > $max) {
+            return false;
+        }
+        return true;
+    }
+    public static function date($date, $format = 'Y-m-d')
+    {
+        $d = DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) === $date;
     }
 }
